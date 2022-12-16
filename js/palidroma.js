af8c36526065ffbaf0bf7e40441827e7ad1a6f5b
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-16 13:25:18 +0100

    fixed up palindrom

--- js/palidroma.js
+++ js/palidroma.js
@@ -1,32 +1,45 @@
 /* Chiedere all’utente di inserire una parola tramiite un form
    Creare una funzione per capire se la parola inserita è palindroma */
 
+
+/--------------------- funzioni -----------------------------/ 
+
+// creare una funzione che capovolge una parola
+function reverseWord(word) {
+    let reverse = '';
+    // console.log(word)
+
+    for(let i = word.length - 1; i >= 0; i-- ) {
+        reverse += word[i];
+    }
+    console.log(reverse);
+
+    let notice = word === reverse ? true : false;
+    return notice;
+}
+
+/--------------------- operazioni preliminari ----------------------/
+
 // 1 - prendere elementi Dom
 const wordElement = document.getElementById('word-user');
 const formElement = document.getElementById('form');
 const noticeElement = document.getElementById('notice');
 
-const wordValue = wordElement.value.trim();
-console.log(wordValue);
+/--------------------- eventi dinamici ----------------------/
 
-// creare una funzione che capovolge una parola
-function reverseWord(word) {
-    let reverse = '';
-    console.log(word)
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const word = (wordElement.value.trim()).toLowerCase();
+    console.log(word);
+    
+    const message = reverseWord(word) ? 'la parola è palindroma' : 'la parola non è palindroma';
+    noticeElement.innerText = message;
+})
 
-    for(let i = word.length - 1; i >= 0; i-- ) {
-        reverse += word[i];
-    }
-    return reverse;
-}
 
-const wordReverse = reverseWord(wordValue);
-console.log(wordReverse);
 
-if (wordValue === wordReverse) {
-    notice = 'la parola è palindroma'
-} else {
-    notice = 'la parola non è palindroma'
-}
 
-noticeElement.innerText = notice;
+
+
+
