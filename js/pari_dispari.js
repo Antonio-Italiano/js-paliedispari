5f4a18d1c5f4a169ec9b50c24af855bd36e8bb59
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-16 14:10:11 +0100

    fixed up pariodispari

--- js/pari_dispari.js
+++ js/pari_dispari.js
@@ -4,73 +4,76 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */ 
 
+
+/------------------ funzioni ------------------/
+// genero il numero random del computer da 1 a 5 con la funzione
+function getRandomNumber(min, max) {
+    max++;
+
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    return randomNumber;
+}
+// stabilisco se la somma e pari o dispari con una funzione
+function isEven(sumNumbers) {
+
+    if(isNaN(sumNumbers % 2 === 0)) {
+        return true;
+    } else { return false; }
+}
+
+/----------------- operazioni preliminari -----------------/
+
 // Elementi DOM
 const playerNumber = document.getElementById('player');
 const selectOption = document.getElementById('select');
 const computerElement = document.getElementById('computer');
 const resultElement = document.getElementById('result');
-const button = document.getElementById('btn');
+const form = document.getElementById('btn');
 
 
 
-
+/----------------- eventi dinamici ---------------/
 
 // event listner bottone
-button.addEventListener('click', function() {
+btn.addEventListener('click', function() {
+
 
     // recupero value 
-    const selectValue = selectOption.value;
-    console.log(selectValue);
-    
-    const numberValue = parseInt(playerNumber.value.trim());
-    console.log(numberValue)
+    const number = parseInt(playerNumber.value.trim());
+    console.log(number)
+
+    const select = selectOption.value;
+    console.log(select);    
 
     // condizione in cui la value ritorna risultati voluti
-    if (numberValue < 1 || numberValue > 5) {
+    if (isNaN(number) || number < 1 || number > 5) {
         alert ('il numero inserito deve essere compreso tra 1 e 5');
+        return;
     }
     
-    // genero il numero random del computer da 1 a 5 con la funzione
-    function getRandomNumber(min, max) {
-        max++;
-
-        const randomNumber = Math.floor(Math.random() * (max - min)) + min;
-        return randomNumber;
+    if (!select || (select !== 'pari' && select !== 'dispari' )) {
+        alert('la scelta non è valida');
+        return;
     }
+
     // assegno il minimo e il massimo della funzione
     const computer = parseInt(getRandomNumber(1, 5)); 
     console.log(computer);
 
     // sommo il numero generato dal computer e lo sommo con quello dell'User
-    const sumNumbers = numberValue + computer;
+    const sumNumbers = number + computer;
     console.log(sumNumbers);
     
-    // stabilisco se la somma e pari o dispari con una funzione
-    function isEven(sumNumbers) {
-
-        if(isNaN(sumNumbers % 2 === 0)) {
-            return true;
-        } else { return false; }
-    }
-    
-    
-    // variabile d'appoggio 
-   let winner;
+    // variabile vittoria
+    const rightselect = isEven(sumNumbers) ? 'pari' : 'dispari';    
     
     // condizioni per stabilire chi ha vinto
-    if (selectValue == 'pari' && isEven(sumNumbers) == true) {
-        console.log('ha vinto l\'User');
-        winner = 'hai vinto';
-    } else if (selectValue == 'dispari' && isEven(sumNumbers) == false) {
-        console.log('ha vinto l\'User');
-        winner = 'hai vinto';
-    } else {
-        winner = 'hai perso';
-    }
+    const winner = select === rightselect ? 'player' : 'computer';
 
     
     // 3 - Stampo in pagina il risultato
-    playerNumber.innerText ='numero player: ' + numberValue ;
-    computerElement.innerText ='numero computer: ' +  computer ;
-    resultElement.innerText = winner ;
+    computerElement.innerText ='il numero del computer: ' +  computer ;
+    resultElement.innerText = 'vince il : ' + winner ;
+
+
 })
